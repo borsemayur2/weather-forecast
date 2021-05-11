e70bd9ced0a1db863dccd68f72457acc29aa847f
Mayur Borse <borsemayur2@gmail.com>
2021-05-11 08:26:26 +0530

    Setup rtk-query

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const OPENWEATHERMAP_API_KEY = import.meta.env
+  .VITE_OPENWEATHERMAP_API_KEY;
+
+export const BASE_URL = "https://api.openweathermap.org/data/2.5/forecast";
--- src/store.js
+++ src/store.js
@@ -1,10 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import weatherReducer from "./weather/weatherSlice";
+import { weatherApi } from "./services/weather";
 
 const store = configureStore({
   reducer: {
     weather: weatherReducer,
+    // Add the generated reducer as a specific top-level slice
+    [weatherApi.reducerPath]: weatherApi.reducer,
   },
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(weatherApi.middleware),
 });
 
 export default store;
